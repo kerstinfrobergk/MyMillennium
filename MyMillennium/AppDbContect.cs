dbf5ee6882b9e876dcb50649b3f4ac6ee82b8f54
Kerstin Kristensen <kerstinkris@hotmail.com>
2026-09-02 14:15:56 +0200

    Add first model and setup EF Core DbContext

---
+++ MyMillennium/AppDbContect.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyMillenniumApi.Data;
+
+namespace MyMillenniumApi
+{
+    public class AppDbContect : DbContext
+    {
+        public AppDbContect(DbContextOptions<AppDbContect> options) : base(options)
+        { }
+
+        public DbSet<ArtItem> ArtItems => Set<ArtItem>();
+    }
+}
---
+++ MyMillennium/Data/ArtItem.cs
@@ -0,0 +1,17 @@
+﻿namespace MyMillenniumApi.Data
+{
+    public class ArtItem
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public Category ItemCategory { get; set; } = Category.Inspiration;
+        public string RawBlobUrl { get; set; } = string.Empty;
+    }
+
+    public enum Category
+    {
+        Inspiration = 0,
+        Profile = 1,
+    }
+}
