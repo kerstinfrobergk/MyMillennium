cbee014ce95452ba6a1e3921a1b87a23c20cd606
Kerstin Kristensen <kerstinkris@hotmail.com>
2026-09-13 18:42:34 +0200

    WIP: Implement initial Blob Storage service

---
+++ MyMillennium/Services/BlobStorageService.cs
@@ -0,0 +1,25 @@
+﻿using Azure.Storage.Blobs;
+
+namespace MyMillenniumApi.Services
+{
+    public class BlobStorageService
+    {
+        //TODO: Implement update and delete methods for blob storage
+
+        private readonly BlobServiceClient _blobServiceClient;
+        private readonly string _containerName;
+        public BlobStorageService(BlobServiceClient blobServiceClient, IConfiguration config)
+        {
+            _blobServiceClient = blobServiceClient;
+            _containerName = config["AzureStorage:ContainerName"] ?? throw new InvalidOperationException("Azure Storage container name is not valid.");
+        }
+
+        public async Task UploadFileAsync(Stream stream, string blobName)
+        {
+            var blobContainerClient = _blobServiceClient.GetBlobContainerClient(_containerName);
+            var blobClient = blobContainerClient.GetBlobClient(blobName);
+
+            await blobClient.UploadAsync(stream, overwrite: true);
+        }
+    }
+}
