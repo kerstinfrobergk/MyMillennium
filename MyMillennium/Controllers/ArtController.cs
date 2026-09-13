67562e24ab0543909bee24cd8b29344a8705af8b
Kerstin Kristensen <kerstinkris@hotmail.com>
2026-09-13 19:15:41 +0200

    Add initial setup for ArtController

---
+++ MyMillennium/Controllers/ArtController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyMillenniumApi.Services;
+
+namespace MyMillenniumApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ArtController : ControllerBase
+    {
+        private readonly BlobStorageService _blobStorageService;
+
+        public ArtController(BlobStorageService blobStorageService)
+        {
+            _blobStorageService = blobStorageService;
+        }
+    }
+}
