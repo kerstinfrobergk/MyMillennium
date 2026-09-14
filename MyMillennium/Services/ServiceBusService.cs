1567f19321b0fc4150086b24e0cdde7f58d85f5c
Kerstin Kristensen <kerstinkris@hotmail.com>
2026-09-14 14:57:29 +0200

    Add Service Bus message sending

--- MyMillennium/Services/ServiceBusService.cs
+++ MyMillennium/Services/ServiceBusService.cs
@@ -5,11 +5,27 @@ namespace MyMillenniumApi.Services
     public class ServiceBusService
     {
         private readonly ServiceBusClient _serviceBusClient;
+        private readonly ServiceBusSender _sender;
 
-        public ServiceBusService(ServiceBusClient serviceBusClient)
+        public ServiceBusService(ServiceBusClient serviceBusClient, IConfiguration config)
         {
             _serviceBusClient = serviceBusClient;
+            var queueName = config["AzureServiceBus:QueueName"] ?? throw new InvalidOperationException("Azure Service Bus queue name could not be found.");
+
+            _sender = _serviceBusClient.CreateSender(queueName);
         }
 
+        public async Task SendProcessArtImageAsync(ProcessArtImage processArtImage)
+        {
+            var messageBody = BinaryData.FromObjectAsJson(processArtImage);
+
+            var message = new ServiceBusMessage(messageBody)
+            {
+                ContentType = "application/json",
+                Subject = nameof(ProcessArtImage),
+            };
+
+            await _sender.SendMessageAsync(message);
+        }
     }
 }
