8b6229f857bdeca89d5afb0d3266bb41d4a28766
Kerstin Kristensen <kerstinkris@hotmail.com>
2026-09-14 11:25:28 +0200

    Add ArtItem upload to db

--- MyMillennium/Controllers/ArtController.cs
+++ MyMillennium/Controllers/ArtController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MyMillenniumApi.Data;
 using MyMillenniumApi.Services;
 
 namespace MyMillenniumApi.Controllers
@@ -7,20 +8,39 @@ namespace MyMillenniumApi.Controllers
     [Route("api/[controller]")]
     public class ArtController : ControllerBase
     {
+        private readonly AppDbContext _dbContext;
         private readonly BlobStorageService _blobStorageService;
 
-        public ArtController(BlobStorageService blobStorageService)
+        public ArtController(AppDbContext dbContext, BlobStorageService blobStorageService)
         {
+            _dbContext = dbContext;
             _blobStorageService = blobStorageService;
         }
 
         [HttpPost("upload")]
-        public async Task<IActionResult> UploadTest(IFormFile file)
+        public async Task<IActionResult> UploadImage(
+            [FromForm] IFormFile file,
+            [FromForm] string title,
+            [FromForm] string description,
+            [FromForm] Category itemCategory)
         {
             using var stream = file.OpenReadStream();
 
-            await _blobStorageService.UploadFileAsync(
-                stream, file.FileName);
+            var filePathExtension = Path.GetExtension(file.FileName);
+            var blobName = $"{Guid.NewGuid()}{filePathExtension}";
+
+            await _blobStorageService.UploadFileAsync(stream, blobName);
+
+            var artItem = new ArtItem()
+            {
+                Title = title,
+                Description = description,
+                ItemCategory = itemCategory,
+                BlobName = blobName
+            };
+
+            _dbContext.ArtItems.Add(artItem);
+            await _dbContext.SaveChangesAsync();
 
             return Ok();
         }
