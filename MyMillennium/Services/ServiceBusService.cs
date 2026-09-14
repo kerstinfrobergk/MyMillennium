bdcbdd61f2651445b5fd18f8648e8082076f520a
Kerstin Kristensen <kerstinkris@hotmail.com>
2026-09-14 14:32:34 +0200

    WIP: Add ServiceBusService

---
+++ MyMillennium/Services/ServiceBusService.cs
@@ -0,0 +1,17 @@
+﻿using Azure.Messaging.ServiceBus;
+
+namespace MyMillenniumApi.Services
+{
+    public class ServiceBusService
+    {
+        private readonly ServiceBusClient _serviceBusClient;
+
+        public ServiceBusService(ServiceBusClient serviceBusClient)
+        {
+            _serviceBusClient = serviceBusClient;
+        }
+
+        // TODO: Add service to DI
+
+    }
+}
