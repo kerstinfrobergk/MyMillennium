4da8dc5adcf338b1f838655d25a5daed05a15b73
Kerstin Kristensen <kerstinkris@hotmail.com>
2026-09-18 14:25:15 +0200

    Add migration for ArtItem processing status

--- MyMillennium/Migrations/20260918121427_AddProcessingStatusToArtItem.Designer.cs
+++ MyMillennium/Migrations/20260918121427_AddProcessingStatusToArtItem.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MyMillenniumApi;
 
@@ -10,9 +11,11 @@ using MyMillenniumApi;
 namespace MyMillenniumApi.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260918121427_AddProcessingStatusToArtItem")]
+    partial class AddProcessingStatusToArtItem
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ MyMillennium/Migrations/20260918121427_AddProcessingStatusToArtItem.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyMillenniumApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddProcessingStatusToArtItem : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ProcessingStatus",
+                table: "ArtItems",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ProcessingStatus",
+                table: "ArtItems");
+        }
+    }
+}
