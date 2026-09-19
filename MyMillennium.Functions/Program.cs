10474d8825d9a6af03445319d0be3341c6577c26
Kerstin Kristensen <kerstinkris@hotmail.com>
2026-09-19 16:13:42 +0200

    WIP: Add art item processing service

--- MyMillennium.Functions/Program.cs
+++ MyMillennium.Functions/Program.cs
@@ -1,9 +1,34 @@
+using Azure.Storage.Blobs;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using MyMillennium.Data.DataAccess;
+using MyMillennium.Functions.Services;
 
 var builder = FunctionsApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddSingleton(sp =>
+{
+    var configuration = sp.GetRequiredService<IConfiguration>();
+
+    var connectionString =
+        configuration["AzureStorage:ConnectionString"]
+        ?? throw new InvalidOperationException(
+            "Azure Storage connection string is missing!");
+
+    return new BlobServiceClient(connectionString);
+});
+
+builder.Services.AddScoped<BlobStorageService>();
+builder.Services.AddScoped<ImageThumbnailService>();
+builder.Services.AddScoped<ProcessArtImageService>();
 
 builder.ConfigureFunctionsWebApplication();
 
--- MyMillennium.Functions/Services/ProcessArtImageService.cs
+++ MyMillennium.Functions/Services/ProcessArtImageService.cs
@@ -7,10 +7,14 @@ namespace MyMillennium.Functions.Services
     public class ProcessArtImageService
     {
         private readonly AppDbContext _dbContext;
+        private readonly BlobStorageService _blobStorageService;
+        private readonly ImageThumbnailService _imageThumbnailService;
 
-        public ProcessArtImageService(AppDbContext dbContext)
+        public ProcessArtImageService(AppDbContext dbContext, BlobStorageService blobStorageService, ImageThumbnailService imageThumbnailService)
         {
             _dbContext = dbContext;
+            _blobStorageService = blobStorageService;
+            _imageThumbnailService = imageThumbnailService;
         }
 
         public async Task ProcessArtItemAsync(ProcessArtImage processArtImage)
@@ -25,6 +29,12 @@ namespace MyMillennium.Functions.Services
             }
 
             // TODO: Retrieve the blob represented by processArtImage.BlobName
+            var originalBlob = await _blobStorageService.DownloadBlobAsync(processArtImage.BlobName);
+
+            if(originalBlob == null)
+            {
+                return;
+            }
 
             // TODO: Generate thumbnail
         }
