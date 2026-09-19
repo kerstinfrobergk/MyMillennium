5fd267d6cf12d10aaf5ebbf9743cf37040fa264e
Kerstin Kristensen <kerstinkris@hotmail.com>
2026-09-19 14:41:58 +0200

    WIP: Add blob download functionality

---
+++ MyMillennium.Functions/Services/BlobStorageService.cs
@@ -0,0 +1,30 @@
+﻿using Azure.Storage.Blobs;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyMillennium.Functions.Services
+{
+    public class BlobStorageService
+    {
+        private readonly BlobServiceClient _blobServiceClient;
+        private readonly string _containerName;
+
+        public BlobStorageService(BlobServiceClient blobStorageService, IConfiguration config)
+        {
+            _blobServiceClient = blobStorageService;
+            _containerName = config["AzureStorage:ContainerName"] ?? throw new InvalidOperationException("Azure Storage container name could not be found.");
+        }
+
+        public async Task<Stream> DownloadBlobAsync(string blobName)
+        {
+            var blobContainerClient = _blobServiceClient.GetBlobContainerClient(_containerName);
+            var blobClient = blobContainerClient.GetBlobClient(blobName);
+
+            var response = await blobClient.DownloadStreamingAsync();
+
+            return response.Value.Content;
+        }
+    }
+}
