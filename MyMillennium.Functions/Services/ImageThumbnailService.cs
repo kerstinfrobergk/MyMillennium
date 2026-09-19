a9344fd69637a29503b9174ac0e493c0856ddd2c
Kerstin Kristensen <kerstinkris@hotmail.com>
2026-09-19 15:45:57 +0200

    WIP: Configure image resize

---
+++ MyMillennium.Functions/Services/ImageThumbnailService.cs
@@ -0,0 +1,26 @@
+﻿using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Processing;
+
+namespace MyMillennium.Functions.Services
+{
+    public class ImageThumbnailService
+    {
+
+        public Stream ResizeImageToThumbnailSize(Stream originalBlob)
+        {
+            var image = Image.Load(originalBlob);
+
+            image.Mutate(x => x.AutoOrient().Resize(new ResizeOptions
+            {
+                Size = new Size(300, 300),
+                Mode = ResizeMode.Crop,
+                PadColor = Color.White,
+                Sampler = KnownResamplers.Bicubic
+            }));
+
+            // TODO: Encode resized image/thumbnail into a new Stream
+
+            // TODO: Returning Stream
+        }
+    }
+}
