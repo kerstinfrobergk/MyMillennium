e859ab84ababcf9a7c77adec070590f5e66289b4
Kerstin Kristensen <kerstinkris@hotmail.com>
2026-09-22 10:42:50 +0200

    Complete art image processing flow

--- MyMillennium.Functions/ProcessArtImageFunction.cs
+++ MyMillennium.Functions/ProcessArtImageFunction.cs
@@ -2,16 +2,18 @@ using Azure.Messaging.ServiceBus;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
 using MyMillennium.Contracts.Messages;
+using MyMillennium.Functions.Services;
 
 namespace MyMillennium.Functions;
 
 public class ProcessArtImageFunction
 {
     private readonly ILogger<ProcessArtImageFunction> _logger;
-
-    public ProcessArtImageFunction(ILogger<ProcessArtImageFunction> logger)
+    private readonly ProcessArtImageService _processArtImageService;
+    public ProcessArtImageFunction(ILogger<ProcessArtImageFunction> logger, ProcessArtImageService processArtImageService)
     {
         _logger = logger;
+        _processArtImageService = processArtImageService;
     }
 
     [Function(nameof(ProcessArtImageFunction))]
@@ -27,6 +29,9 @@ public class ProcessArtImageFunction
         var processArtImage = message.Body.ToObjectFromJson<ProcessArtImage>();
 
         _logger.LogInformation($"Message retrieved from queue. Processing ArtItem: {processArtImage?.ArtItemId}, BlobName: {processArtImage?.BlobName}");
+
+        //TODO: Add error handling if processArtImage is null
+        await _processArtImageService.ProcessArtItemAsync(processArtImage);
 
         await messageActions.CompleteMessageAsync(message);
     }
